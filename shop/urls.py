4c3ef93051c8f6da35899824c88b4c736f3a9085
mehdi <mehdi.benbouchaib@gmail.com>
2022-04-13 23:10:34 +0000

    start project

---
+++ shop/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path
+from store.views import index
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('admin/', admin.site.urls),
+]
---
+++ store/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+class product(models.Model):
+    name = models.CharField(max_length=128)
+    price = models.FloatField(default=0.0)
+    stock = models.IntegerField(default=0)
+    description = models.TextField(blank=True)
+    thumbnail = models.ImageField(upload_to="products", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}({self.stock})"
