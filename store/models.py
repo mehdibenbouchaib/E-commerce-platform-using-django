061b33c393616578e41954edcdce176e2658333a
mehdi <mehdi.benbouchaib@gmail.com>
2022-05-01 18:08:17 +0000

    add main file

--- store/models.py
+++ store/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
+
 class Product(models.Model):
     name = models.CharField(max_length=128)
     slug = models.SlugField(max_length=128)
