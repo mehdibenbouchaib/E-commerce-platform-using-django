27387e968dcb69ba9df1c012e5c2b7d5586eb3b0
mehdi <mehdi.benbouchaib@gmail.com>
2022-05-03 22:15:09 +0000

    add button to signup

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from accounts.views import signup, logout_user, login_user
 from store.views import index, product_detail
 
 from shop import settings
@@ -8,5 +9,8 @@ from shop import settings
 urlpatterns = [
                   path('', index, name='index'),
                   path('admin/', admin.site.urls),
+                  path('signup/', signup, name="signup"),
+                  path('logout/', logout_user, name="logout"),
+                  path('login/', login_user, name="login"),
                   path('product/<str:slug>', product_detail, name="product"),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
