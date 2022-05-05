c965cbc9973206b56ccabd50815ba031c4507b55
mehdi <mehdi.benbouchaib@gmail.com>
2022-05-05 13:01:03 +0000

    add commend file

--- store/models.py
+++ store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 
+from shop.settings import AUTH_USER_MODEL
+
 
 class Product(models.Model):
     name = models.CharField(max_length=128)
@@ -15,3 +17,25 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse("product", kwargs={"slug": self.slug})
+
+
+class Order(models.Model):
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.product.name} ({self.quantity})"
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    orders = models.ManyToManyField(Order)
+    ordered = models.BooleanField(default=False)
+    ordered_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
