944c6815f085e6ce3d0f9114f95ff191c10988c5
mehdi <mehdi.benbouchaib@gmail.com>
2022-10-16 12:14:51 +0100

    final version

--- shop/urls.py
+++ shop/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from accounts.views import signup, logout_user, login_user
-from store.views import index, product_detail
+from store.views import index, product_detail, cart,  add_to_cart, delete_cart
 
 from shop import settings
 
@@ -12,5 +12,8 @@ urlpatterns = [
                   path('signup/', signup, name="signup"),
                   path('logout/', logout_user, name="logout"),
                   path('login/', login_user, name="login"),
+                  path('cart/', cart, name="cart"),
+                  path('cart/delete/', delete_cart, name="delete-cart"),
                   path('product/<str:slug>', product_detail, name="product"),
+                  path('product/<str:slug>/add-to-cart/', add_to_cart, name="add-to-cart"),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,16 +1,51 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from audioop import reverse
+from django.urls import reverse
+from audioop import reverse as audio_reverse
+from django.shortcuts import render, get_object_or_404, redirect
 
 from django.shortcuts import render
 
-from store.models import Product
+from store.models import Product, Cart, Order
 
 
 def index(request):
-    product = Product.objects.all()
-    return render(request, 'store/index.html', context={"products": product})
+    products = Product.objects.all()
+    return render(request, 'store/index.html', context={"products": products})
 
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
-    return render(request, 'store/detail.html', context={"product":product})
+    return render(request, 'store/detail.html', context={"product": product})
+
+
+def add_to_cart(request, slug):
+    user = request.user
+    product = get_object_or_404(Product, slug=slug)
+    cart, _ = Cart.objects.get_or_create(user=user)
+    order, created = Order.objects.get_or_create(user=user,
+                                                 ordered=False,
+                                                 product=product)
+
+    if created:
+        cart.orders.add(order)
+        cart.save()
+    else:
+        order.quantity += 1
+        order.save()
+
+    return redirect(reverse("product", kwargs={"slug": slug}))
+
+
+def cart(request):
+    cart = get_object_or_404(Cart, user=request.user)
+    # print(cart.__dict__, "--------------------------------------------------------------------")
+    return render(request, 'store/cart.html', context={"orders": cart.orders.all()})
+
+
+def delete_cart(request):
+    cart = request.user.cart
+    if cart:
+        cart.delete()
+
+    return redirect('index')
+
